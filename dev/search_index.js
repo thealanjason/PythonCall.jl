78168b00e1840b1c31552e95d55d16cd1dce7304
Documenter.jl <documenter@juliadocs.github.io>
2021-01-25 12:17:38 +0000

    build based on d70608d

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Python.jl","page":"Python.jl","title":"Python.jl","text":"","category":"section"},{"location":"","page":"Python.jl","title":"Python.jl","text":"Optimistic placeholder.","category":"page"}]
+}
